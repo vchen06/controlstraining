d627e36e29f32b1f764f7ce66d1414f0b15bc531
Vivien Chen <vivienchen@chenwei-a03.vmware.com>
2022-10-27 20:10:50 -0700

    hi

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.XboxController;
@@ -15,19 +16,27 @@ import edu.wpi.first.wpilibj.Timer;
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   private final CANSparkMax motor = new CANSparkMax(17, MotorType.kBrushless);
-
   private final CANSparkMax motor2 = new CANSparkMax(1, MotorType.kBrushless);
+  //flywheels
+  private final CANSparkMax motor3 = new CANSparkMax(3, MotorType.kBrushless);
+  private final CANSparkMax motor4 = new CANSparkMax(16, MotorType.kBrushless);
+
 
   private final XboxController controller = new XboxController(0);
 
   private final Timer timer = new Timer();
 
-  private final AnalogPotentiometer analog = new AnalogPotentiometer(0);
+  private final AnalogPotentiometer analog1 = new AnalogPotentiometer(0);
+  private final AnalogPotentiometer analog2 = new AnalogPotentiometer(1);
+  private final AnalogPotentiometer analog3 = new AnalogPotentiometer(2);
 
   public ExampleSubsystem() {
     motor.restoreFactoryDefaults();
     motor2.restoreFactoryDefaults();
     motor2.follow(motor);
+    motor.setIdleMode(IdleMode.kCoast);
+    motor2.setIdleMode(IdleMode.kCoast);
+
     timer.start();
   }
 
@@ -36,10 +45,23 @@ public class ExampleSubsystem extends SubsystemBase {
   
   boolean run = false;
 
+  boolean stored = false;
+
   @Override
   public void periodic() {
-    System.out.println(analog.get());
+    System.out.println(analog1.get());
+    System.out.println(analog2.get());
+    System.out.println(analog3.get());
+    System.out.println("_");
+    // if (analog1.get() >= n) {
+    //   motor.set(1);
+    // } 
 
+
+
+
+    // System.out.println(analog.get());
+    // motor.set(1-analog.get()/0.64);
 
     //TIMER
     // if (timer.get()<4) {
